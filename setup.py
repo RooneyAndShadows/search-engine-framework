d33424df7e70e30ae74b4a20151438f0249e9ceb
Kiril Lazarov <trickingmachine@gmail.com>
2020-09-05 16:44:23 +0300

    pip setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(name='easy_search',
+      version='1.0',
+      description='Simple search engine framework',
+      author='Kiril Lazarov',
+      author_email='krlazarov93@gmail.com',
+      url='https://github.com/ShadowSteps/search-engine-framework',
+      packages=['easy_search'],
+      package_dir = {'': 'python3'}
+     )
